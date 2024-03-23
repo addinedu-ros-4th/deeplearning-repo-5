abbc8713e702940fb3d1c7242ff405528ceaaedc
AUTO-KKYU <dknjy3313@gmail.com>
2024-03-23 17:30:17 +0900

    client to client files

--- src/kdk/TCPIP/Client.py
+++ src/kdk/TCPIP/Client.py
@@ -1,15 +1,6 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtGui import QImage, QPixmap
-from PyQt6 import uic
-from PyQt6.QtCore import QThread, pyqtSignal
 import socket
-import numpy as np
 import cv2
-
-
-# Load UI file
-from_class = uic.loadUiType("/home/kkyu/amr_ws/DL/project_deep/face_communication/communication.ui")[0]
+import numpy as np
 
 def recvall(sock, count):
     buf = b''
@@ -21,47 +12,43 @@ def recvall(sock, count):
         count -= len(newbuf)
     return buf
 
-class VideoThread(QThread):
-    change_pixmap_signal = pyqtSignal(np.ndarray)
+def run_client():
+    TCP_IP = '192.168.0.29'
+    TCP_PORT = 3008
 
-    def run(self):
-        TCP_IP = '127.0.1.1'
-        TCP_PORT = 3001
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((TCP_IP, TCP_PORT))
+    cap = cv2.VideoCapture(0)  # Open local webcam
 
-        while True:
-            length = int(s.recv(16).strip())
-            print("Receiving frame of size:", length)
-            
-            frame_string = recvall(s, length)
-            
-            frame_array = np.frombuffer(frame_string, dtype=np.uint8)
-            frame = cv2.imdecode(frame_array, cv2.IMREAD_COLOR)
-            
-            self.change_pixmap_signal.emit(frame)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-class WindowClass(QMainWindow, from_class):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
+        frame_string = cv2.imencode('.jpg', frame)[1].tobytes()  # Change tostring() to tobytes()
+        size = len(frame_string)
+        s.sendall(str(size).ljust(16).encode())
+        s.sendall(frame_string)
 
-        self.video_thread = VideoThread()
-        self.video_thread.change_pixmap_signal.connect(self.update_image)
-        self.video_thread.start()
+        length_header = s.recv(16)
+        if not length_header:
+            break
+        length = int(length_header.strip())
+        print("Receiving frame of size:", length)
+        
+        frame_string = recvall(s, length)
+        
+        frame_array = np.frombuffer(frame_string, dtype=np.uint8)
+        frame = cv2.imdecode(frame_array, cv2.IMREAD_COLOR)
+        
+        cv2.imshow('Received', frame)  # Show received frame
+        
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
 
-    def update_image(self, frame):
-        qt_img = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format.Format_BGR888)
-        pixmap = QPixmap.fromImage(qt_img)
-        self.label.setPixmap(pixmap)
-
-    def closeEvent(self, event):
-        self.video_thread.quit()
-        self.video_thread.wait()
+    s.close()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    myWindows = WindowClass()
-    myWindows.show()
-    sys.exit(app.exec())
+    run_client()
--- src/kdk/TCPIP/Server.py
+++ src/kdk/TCPIP/Server.py
@@ -13,8 +13,8 @@ def recvall(sock, count):
     return buf
 
 def run_server():
-    TCP_IP = '127.0.1.1'
-    TCP_PORT = 3001
+    TCP_IP = '192.168.0.29'
+    TCP_PORT = 3008
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((TCP_IP, TCP_PORT))
@@ -24,25 +24,30 @@ def run_server():
     conn, addr = s.accept()
     print('Connected with', addr)
 
-    cap = cv2.VideoCapture('/dev/video0')
+    cap = cv2.VideoCapture(0)  # Open local webcam
 
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        frame_string = cv2.imencode('.jpg', frame)[1].tostring()
+        frame_string = cv2.imencode('.jpg', frame)[1].tobytes()  # Change tostring() to tobytes()
         size = len(frame_string)
         conn.sendall(str(size).ljust(16).encode())
         conn.sendall(frame_string)
 
-        length = int(conn.recv(16).strip())
+        length_header = conn.recv(16)
+        if not length_header:
+            break
+        length = int(length_header.strip())
         print("Receiving frame of size:", length)
         
         frame_string = recvall(conn, length)
         
         frame_array = np.frombuffer(frame_string, dtype=np.uint8)
         frame = cv2.imdecode(frame_array, cv2.IMREAD_COLOR)
+        
+        cv2.imshow('Received', frame)  # Show received frame
         
         if cv2.waitKey(1) & 0xFF == 27:
             break
